82d68e067db77a8950afc21f9fa6a36472272fbb
ByeonSeungHa <a01051205048@gmail.com>
2022-01-28 15:30:04 +0900

    2022-01-28

---
+++ python/ch3-6.py
@@ -0,0 +1,8 @@
+choice(seq) : 비어 있지 않은 sequence에서 임의의 요소를 선택한다.
+choices(population, k) : 모집단으로부터 k의 크기를 갖는 목록을 선택한다.
+randint(a, b) : [a, b] 범위에서 난수 정수를 반환한다.
+random(...) : 0~1 사이의 난수 실수를 반환한다.
+sample(population, k) : 모집단(Population)으로 부터 k개를 임의 추출한다.
+seed() : 종자(seed)값을 지정하여 동일한 난수가 선택되도록 한다.
+uniform(a, b) : [a, b] 범위의 난수 실수를 균등하게 반환한다.
+normalvariate(mu,sigma)6) : 정규 분포(Normal distribution)를 따르는 난수를 반환한다.(mu는 평균이고 sigma는 표준 편차를 의미한다.)
