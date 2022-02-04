8829f2a8eaf279b6f5564feca72d2afb78eb34c3
ByeonSeungHa <a01051205048@gmail.com>
2022-02-04 14:43:26 +0900

    2022-02-04

---
+++ python/ch5-2(모듈).py
@@ -0,0 +1,35 @@
+ adx(x) : x의 절대값을 반환한다.
+ all(iterable) : 모든 요소가 True 일 때 True를 반환한다.
+ any(iterable) : 하나 이상의 요소가 True 일 때 True를 반환한다.
+ bin(number) : 10진수 정수를 2진수로 반환한다.
+ bool(x) : x를 논리형으로 반환한다.
+ complex(x) : x를 복소수형으로 반환한다.
+ dict(iterable) : 사전형 자료구조 형으로 반환한다.
+ dir(x) : 변수, 내장함수, 내장클래스의 목록을 반환한다:
+ enumerate(iterable) : 열거형 자료를 순회하여 색인과 값을 반환한다.
+ eval(expr) : 문자열 수식을 계산 가능한 수식으로 반환한다.
+ float(x) : 실수로 형 변환하여 반환한다.
+ format(value) : value에 양식을 적용한다.
+ help(x) : 함수 또는 클래스의 도움말을 반환한다.
+ hex(number) : 10진수 정수를 16진수 값을 반환한다.
+ id(obj) : 객체의 주소를 반환한다.
+ input([prompt]) : 키보드로 입력한 문자열을 반환한다.
+ int(x) : x를 정수형으로 반환한다.
+ len(obj) : 전체 원소의 길이를 반환한다.
+ list(iterable) : list 자료구조로 형 변환한다.
+ max(iterable) : 최댓값을 반환한다.
+ min(iterable) : 최솟값을 반환한다.
+ oct(number) : 10진수 정수를 8진수 값으로 반환한다.
+ open(file, mode) : 파일 읽기와 쓰기
+ ord(character) : character를 아스키 값으로 반환한다.
+ pow(x, y) : x에 대한 y의 제곱을 계산하여 반환한다.
+ print(value) : value를 콘솔에 출력한다.
+ range(n) : 0에서 n-1 사이의 정수를 반환한다.
+ round(number) : 실수 number를 대상으로 반올림을 수행한다.
+ set(iterable) : 셋 자료구조로 형 변환한다.
+ sorted(iterable) : 반복 가능한 원소들을 대상으로 정렬한다.
+ str(object) : 문자형 자료형으로 변환한다.
+ sum(iterable) : 숫자들의 합을 구한다.
+ tuple(iterable) : 튜플 자료구조로 형 변환한다.
+ type(x) : x의 자료형을 반환한다.
+ zip(iterable*) : iterable의 원소를 묶어서 반환한다.
