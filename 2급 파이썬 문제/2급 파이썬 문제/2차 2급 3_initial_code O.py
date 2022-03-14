5a3ba4da10cc07c40242dba459e013bfde202352
ByeonSeungHa <a01051205048@gmail.com>
2022-03-15 07:17:12 +0900

    2022-03-15

--- 2급 파이썬 문제/2급 파이썬 문제/2차 2급 3_initial_code O.py
+++ 2급 파이썬 문제/2급 파이썬 문제/2차 2급 3_initial_code O.py
@@ -4,6 +4,12 @@
 def solution(N, M):
     #여기에 코드를 작성해주세요.
     answer = 0
+    for i in range(N,M+1,2):
+        if N < M:
+            answer += i**2
+        else:
+            return False
+
     return answer
 
 #아래는 테스트케이스 출력을 해보기 위한 코드입니다.
