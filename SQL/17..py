5ac64103d8201848ec499e6bea0ec49f42e2ca61
ByeonSeungHa <a01051205048@gmail.com>
2022-03-15 16:59:08 +0900

    2022-03-15

---
+++ SQL/17..py
@@ -0,0 +1,8 @@
+# 다음 중  데이터를 조회할 때 빠른 성능을 낼 수 있도록 하기 위해 원래 속성의 값을 계산하여
+# 저장할 수 있도록 만든 속성으로 가장 적절한 것은?
+#
+# 1. 파생속성(Derived Attribute)
+# 2. 기본속성(Basic Attribute)
+# 3. 설계속성(Designed Attribute)
+# 4. PK속성(Derived Attribute)
+# 답 1
