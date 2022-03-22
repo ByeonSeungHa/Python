7ace8010f78b8c991c2c853e69996dc2c73e97e4
ByeonSeungHa <a01051205048@gmail.com>
2022-03-23 06:50:05 +0900

    2022-03-23

---
+++ Busan Study/52.py
@@ -0,0 +1,61 @@
+# 문제
+#
+# def solution(number):
+#     count = 0
+#     for i in range(1, number + 1):
+#         current = i
+#         temp = count
+#         while current != 0:
+#             if @@@:
+#                 count += 1
+#                 print("pair", end = '')
+#             current = current // 10
+#         if temp == count:
+#             print(i, end = '')
+#         print(" ", end = '')
+#     print("")
+#     return count
+#
+# #The following is code to output testcase.
+# number = 40
+# ret = solution(number)
+#
+# #Press Run button to receive output.
+# print("Solution: return value of the function is", ret, ".")
+
+# 내 답
+#
+# def solution(number):
+#     count = 0
+#     for i in range(1, number + 1):
+#         current = i
+#         temp = count
+#         while current != 0:
+#             if current % 3 == 0:
+#                 count += 1
+#                 print("pair", end = '')
+#             current = current // 10
+#         if temp == count:
+#             print(i, end = '')
+#         print(" ", end = '')
+#     print("")
+#     return count
+
+# 풀이
+
+def solution(number):
+    count = 0
+    for i in range(1, number + 1):
+        current = i
+        temp = count
+        while current != 0:
+            if current % 10 == 3 or current % 10 == 6 or current % 10 == 9:
+                count += 1
+                print("pair", end = '')
+            current = current // 10
+        if temp == count:
+            print(i, end = '')
+        print(" ", end = '')
+    print("")
+    return count
+
