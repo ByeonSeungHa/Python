f0a084f60dc5a7bd2c498499907fbf116e5c08f1
ByeonSeungHa <a01051205048@gmail.com>
2022-04-06 07:15:45 +0900

    2022-04-06

---
+++ SQL/6..py
@@ -0,0 +1,14 @@
+# 다음 중 ANSI-SPARC에서 정의한 3단게구조(three-level architecture)에서 아래 내용이 설명하는
+# 스키마 구조로 적절한 것은?
+#
+# 아래
+# - 모든 사용자 관점을 통합한 조직 전체 관점의 통합적 표현
+# - 모든 응용시스템들이나 사용자들이 필요로 하는 데이터를 통합한 조직 전체의 DB를 기술한 것으로
+# DB에 저장되는 데이터와 그들간의 관계를 표현하는 스키마
+#
+# 1. 외부스키마(External Schema)
+# 2. 개념스키마(Conceptual Schema)
+# 3. 내부스키마(Internal Schema)
+# 4. 논리스키마(Logical Schema)
+#
+# 답 2
