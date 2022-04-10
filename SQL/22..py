ed1da1af4e65e1e6be86f9c87a5f08eff13aa36e
ByeonSeungHa <a01051205048@gmail.com>
2022-04-11 07:04:40 +0900

    2022-04-11

---
+++ SQL/22..py
@@ -0,0 +1,8 @@
+# 다음 중 엔터티간의 관계에서 1:1, 1:M과 같이 관계의 기수성을 나타내는 것으로 가장 적절한 것은? # 22
+#
+# 1. 관계명(Relationship Membership)
+# 2. 관계차수(Relationship Degree/Cardinality)
+# 3. 관계선택사양(Relationship Optionality)
+# 4. 관계정의(Relationship Definition)
+#
+# 답 2
