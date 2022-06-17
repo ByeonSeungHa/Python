c74bcbda85504d7b56af0db5044f412a54cd1a0d
ByeonSeungHa <a01051205048@gmail.com>
2022-06-17 15:42:02 +0900

    2022-06-17

--- Alogorithm/2. Greedy 연습.py
+++ Alogorithm/2. Greedy 연습.py
@@ -1,8 +1,8 @@
-n = 3250
-cnt = 0
-arr = [1000, 500, 100, 50]
-
-for coin in arr:
-    cnt += n//coin
-    n%=coin
-print(cnt)
+# n = 3250
+# cnt = 0
+# arr = [1000, 500, 100, 50]
+#
+# for coin in arr:
+#     cnt += n//coin
+#     n%=coin
+# print(cnt)
